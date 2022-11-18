f14dbe45e572478326179f3be8b47a5c101672dd
niieeer <clement0201@icloud.com>
2022-11-18 17:07:33 +0100

    Hello

---
+++ src/pages/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Error = () => {
+    return (
+        <div>
+            <Navigation />
+            <p>404 page not found</p>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+    return (
+        <div className='ac'>
+            <h1>Acceuil</h1>
+            <Navigation />
+        </div>
+    );
+};
+
+export default Home;
