88572af98545603c4d8966969c34d3ad7812e077
niieeer <clement0201@icloud.com>
2022-12-28 10:30:49 +0100

    style for perk

--- src/components/Perk.js
+++ src/components/Perk.js
@@ -7,25 +7,33 @@ const Perk = () => {
             <div className='perk-description'>
                 <ul>
                     <li>
-                        ProjectSchool
-                        <p>Projet visant à ce que prof et éleve puissent consulter les notes ajouté.
-                            C'est un travail réalisé en duo !
-                        </p>
+                        <div>
+                            ProjectSchool
+                            <p>Projet visant à ce que prof et éleve puissent consulter les notes ajouté.
+                                C'est un travail réalisé en duo !
+                            </p>
+                        </div>
                         <a href="https://github.com/niieeer/ProjectSchool-Front">Github</a>
                     </li>
                     <li>
-                        NoodCat
-                        <p>Le but de l'application est de pouvoir réserver une table dans un bar à chat.</p>
+                        <div>
+                            NoodCat
+                            <p>Le but de l'application est de pouvoir réserver une table dans un bar à chat.</p>
+                        </div>
                         <a href="https://github.com/niieeer/NoodCat">Github</a>
                     </li>
                     <li>
-                        Dentiio
-                        <p>Le client peut consulter ses avancées médicales ainsi que prendre rendez-vous et parler avec son dentiste.</p>
+                        <div>
+                            Dentiio
+                            <p>Le client peut consulter ses avancées médicales ainsi que prendre rendez-vous et parler avec son dentiste.</p>
+                        </div>
                         <a href="https://github.com/PaulineMahut/dentiio">Github</a>
                     </li>
                     <li>
-                        LookMovies
-                        <p>Le site à pour but de consulter tout les films du moments à l'aide d'une recherche avec l'API TMDb </p>
+                        <div>
+                            LookMovies
+                            <p>Le site à pour but de consulter tout les films du moments à l'aide d'une recherche avec l'API TMDb </p>
+                        </div>
                         <a href="https://github.com/niieeer/tp_film">Github</a>
                     </li>
                 </ul>
