e8783719a82578143f8d8b47d10874fb7f17e3bc
niieeer <clement0201@icloud.com>
2022-12-29 11:32:15 +0100

    style contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,12 @@ const Contact = () => {
     return (
         <div className='contact-container'>
             <h2>Démarrons un projet ensemble, n’hésitez pas à me contacter</h2>
-            <input type="text" value={'Nom'}/>
-            <input type="text" value={'Adresse e-mail'}/>
-            <textarea value={'Votre message'}></textarea>
-            <input type="button" value="Me contacter" />
+            <form action="">
+                <input type="text" placeholder='Nom' minLength="1" maxLength="20" required />
+                <input type="email" placeholder='Adresse e-mail' maxLength="50" required />
+                <textarea placeholder='Votre message' required maxLength="1000"></textarea>
+                <input type="submit" value="Me contacter" />
+            </form>
         </div>
     );
 };
