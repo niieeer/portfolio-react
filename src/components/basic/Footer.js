75d3da4a50365d8a6a97fd2d704d67e142237037
niieeer <clement0201@icloud.com>
2023-04-15 13:22:34 +0200

    footer css

--- src/components/basic/Footer.js
+++ src/components/basic/Footer.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Title from './Title';
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <footer>
             <p>© Beaudouin Clément. - Tous droits réservés.</p>
-            <a href="">LindekIn</a>
-            <a href="">Github</a>
-            <a href="">LindekIn</a>
+            <div className='flex-icon'>
+                <a href="https://www.linkedin.com/in/clément-beaudouin-1b445b250/">
+                    <FaLinkedin />
+                </a>
+
+                <a href="https://github.com/niieeer">
+                    <FaGithub />
+                </a>
+            </div>
         </footer>
     );
 };
